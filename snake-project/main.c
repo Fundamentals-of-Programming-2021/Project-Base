c26c940d979cf9bb501c235501631149459ae982
mhbahmani <mhbahmani79@gmail.com>
2022-01-12 16:17:51 +0330

    minor fix ad CMakeLists.txt

--- snake-project/main.c
+++ snake-project/main.c
@@ -79,8 +79,8 @@ int main() {
     	filledCircleRGBA(renderer, snake_x, snake_y, snake_radius, 0, 100, 100, 255);
     	filledEllipseRGBA(renderer, food_x, food_y, food_rx, food_ry, rand() % 255, rand() % 255, rand() % 255, 255);
         char* buffer = malloc(sizeof(char) * 50);
-        sprintf(buffer, "score: %d   elapsed time: %dms", snake_score, start_ticks - begining_of_time);
-        printf("%s", buffer);
+	sprintf(buffer, "score: %d   elapsed time: %dms", snake_score, start_ticks - begining_of_time);
+        /* printf("%s", buffer); */
         stringRGBA(renderer, 5, 5, buffer, 0, 0, 0, 255);
     	SDL_RenderPresent(renderer);
 
